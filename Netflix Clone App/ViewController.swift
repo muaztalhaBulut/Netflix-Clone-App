4865bc51ced62fa6a9c1f0f7117fea2a6ff32cbd
Muaz Talha BULUT <84229657+muaztalhaBulut@users.noreply.github.com>
2022-04-30 13:10:08 +0300

    Initial Commit

---
+++ Netflix Clone App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone App
+//
+//  Created by Muaz Talha Bulut on 30.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
